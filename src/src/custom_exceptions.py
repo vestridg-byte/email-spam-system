24186fb3c1cf5cac4de417a2cc74f6f2a6e5da49
Snoopy415420 <rleomo@stevens.edu>
2026-05-01 00:03:52 +0800

    Delete src/src directory

--- src/src/custom_exceptions.py
+++
@@ -1,7 +0,0 @@
-class DatasetFileError(Exception):
-    """Raised when the dataset file cannot be found or loaded."""
-    pass
-
-class DatasetColumnError(Exception):
-    """Raised when required column are missing from the dataset."""
-    pass
